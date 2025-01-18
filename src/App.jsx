8c4ef72badaf05576b5e9ae804e296ff9639478f
RohitZe <rohithsharmaa105@gmail.com>
2025-01-19 00:10:23 +0530

    everthing configured

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Body from './components/Body'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
       <div>
-       
-      <h1>let,s revise all fundamentals of react</h1>
-     
+
+       <Body/>
       </div>
 
     </>
---
+++ src/components/Browse.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Browse = () => {
+  return (
+    <div className='w-40 bg-black  text-xl'>
+       hello
+
+    </div>
+  )
+}
+
+export default Browse
